0e74bfdf7544f6f31240374b0b1f57d61e62cc71
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-04-22 18:24:04 +0900

    Commit  changes

--- components/chat-sidebar-components/chat-main-sidebar.tsx
+++ components/chat-sidebar-components/chat-main-sidebar.tsx
@@ -12,8 +12,6 @@ export default async function ChatMainSidebar() {
         return redirect('/');
     }
 
-
-
     return (
         <ul className='space-y-4 flex flex-col items-center h-full text-primary
         w-full dark:bg-[#1E1F22] py-3'>
