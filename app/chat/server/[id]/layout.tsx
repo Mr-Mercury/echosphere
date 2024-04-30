613566336206ff67f3a06aecd2b2cf9a1e354800
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-04-30 17:06:21 +0900

    Changed to render channel sidebar in dynamic server route layout

---
+++ app/chat/server/[id]/layout.tsx
@@ -0,0 +1,38 @@
+import { getUser } from "@/lib/utils/data/fetching/userData";
+import { getServerById } from "@/lib/utils/data/fetching/serverData";
+import { generateRandomChannels } from "@/lib/utils/mocking/mock";
+import { redirect } from "next/navigation";
+import { ChannelSideBar } from "@/components/contents-sidebar-components/channel-sidebar/channel-sidebar";
+
+
+const ChannelLayout = async ({ children, params }: {
+    children: React.ReactNode,
+    params: {id:string},
+}) => {
+
+    const user = await getUser('1234');
+
+    // if (!user) {
+    //     return redirectsignin()
+    // }
+
+    const server = await getServerById(params.id);
+
+    const channels = await generateRandomChannels(5);
+
+    if (!server) {
+        return redirect('/')
+    }
+
+    return (
+        <div className='h-full'>
+            <div className='hidden md:flex h-full w-60
+            z-20 flex-col fixed inset-y-0'>
+                <ChannelSideBar channels={channels} server={params.id}/>
+            </div>
+            <main className='h-full md:pl-60'>{children}</main>
+        </div>
+    )
+}
+
+export default ChannelLayout;
---
+++ app/chat/server/personal/layout.tsx
@@ -0,0 +1,11 @@
+const ChannelLayout = ({ children }: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div> 
+            {children}
+        </div>
+    )
+}
+
+export default ChannelLayout;
