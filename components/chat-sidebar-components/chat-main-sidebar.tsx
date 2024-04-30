35d53301507ef2349e4655e339da22852e172a8f
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-04-30 16:35:38 +0900

    css for nav sidebar, selected server tic/css complete

--- components/chat-sidebar-components/chat-main-sidebar.tsx
+++ components/chat-sidebar-components/chat-main-sidebar.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation'
 import Link from 'next/link';
 import { getUser } from '@/lib/utils/data/fetching/userData';
 import ChatNewButton from './chat-new-button/chat-new-button';
+import ChatNavItem from './chat-nav-item';
 
 // Later on, pass with button component
 export default async function ChatMainSidebar() {
@@ -16,16 +17,17 @@ export default async function ChatMainSidebar() {
     }
 
     return (
-        <div className='space-y-4 flex flex-col items-center h-full text-primary
+        <div className='space-y-4 flex flex-col items-center h-full text-white
         bg-[#1E1F22] py-3'>
-            <ul>
-                {Array.from(user.servers).map(server => (
-                    <li key={server.id}>
-                        <Link href={`/chat/server/${server.id}`}>{server.name}</Link>
-                    </li>
-                ))}
-            </ul>
-            <ScrollArea ></ScrollArea>
+            <ScrollArea className='flex-1 w-full'>
+                <ul className='mb-4'>
+                    {Array.from(user.servers).map(server => (
+                        <li key={server.id}>
+                            <ChatNavItem id={server.id} name={server.name} image={server.image}/>
+                        </li>
+                    ))}
+                </ul>
+            </ScrollArea>
             <Separator className='h-[2px] bg-zinc-600   
             rounded-md w-10 mx-auto'/>
             <ChatNewButton />
