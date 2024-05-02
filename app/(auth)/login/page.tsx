5fe089d70e6eeb629982e5307c13714ca8acd966
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-02 17:58:08 +0900

    Buttons added to login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { AuthButton } from "@/components/islets/auth/auth-button";
+import { AuthCardWrapper } from "@/components/islets/auth/auth-card-wrapper";
 import { cn } from "@/lib/utils";
 import { LoginForm } from "@/components/auth-components/login-form";
 
 const LoginPage = () => {
     return (
-        <LoginForm>
-
-        </LoginForm>
+        <AuthCardWrapper headerLabel='Login Here' 
+        backButtonLabel="Don't have an account?" 
+        backButtonHref='/registration'
+        showSocial={true}>
+            <LoginForm>
+            </LoginForm>
+        </AuthCardWrapper>
     )
 }
 
