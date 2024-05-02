3d86f80b0b2614625e1f4ee2204e08c49469ada3
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-02 16:57:47 +0900

    Login button work

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { AuthButton } from "@/components/islet/auth/auth-button";
+import { cn } from "@/lib/utils";
+
 const LoginPage = () => {
     return (
-        <div>
-            LOGIN HERE
-        </div>
+        <main className='flex h-full h-screen flex-col items-center 
+        justify-center bg-[#313338] text-white'>
+            <div className='flex flex-col items-center space-y-6'>
+                <h1 className='text-6xl font-semibold text-white drop-shadow-md'>
+                    Auth
+                </h1>
+                <p className='text-white text-lg'>
+                    Login to your Echosphere Account
+                </p>
+                <div>
+                    <AuthButton>
+                        <Button variant='secondary' size='lg'>
+                            Sign In
+                        </Button>
+                    </AuthButton>
+                </div>
+            </div>
+        </main>
     )
 }
 
