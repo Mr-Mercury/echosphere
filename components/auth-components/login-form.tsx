672ce79ad55183dd810fbd24f19d10fcac7ea1db
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-02 19:14:59 +0900

    Login form work

--- components/auth-components/login-form.tsx
+++ components/auth-components/login-form.tsx
@@ -4,9 +4,10 @@ import * as z from 'zod';
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { LoginSchema } from '@/schemas';
+import { Input } from '../ui/input';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 
-export const LoginForm = () => {
+export const LoginForm = ({}) => {
 
     const form = useForm<z.infer<typeof LoginSchema>>({
         resolver: zodResolver(LoginSchema),
@@ -17,8 +18,37 @@ export const LoginForm = () => {
     }); 
 
     return (
-        <div>
-            Login Form
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})}
+            className='space-y-6'>
+                <div className='space-y-4'>
+                    <FormField control={form.control} name='email' 
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input {...field}
+                                placeholder='mail@mailtown.com'
+                                type='email'
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem> 
+                    )}/>
+                    <FormField control={form.control} name='password' 
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input {...field}
+                                placeholder='********'
+                                type='password'
+                                />
+                            </FormControl>
+                        </FormItem> 
+                    )}/>
+                </div>
+            </form>
+        </Form>
     )
 }
