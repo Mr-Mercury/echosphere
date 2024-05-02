db16a2fc3529bb6fd254fefd60aaf6e5424690b1
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-02 17:58:08 +0900

    Buttons added to login

---
+++ components/islets/auth/auth-card-wrapper.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
+import { AuthHeader } from "./auth-header";
+import { AuthSocial } from "@/components/auth-components/auth-social";
+
+interface AuthCardWrapperProps {
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+}
+
+export const AuthCardWrapper = ({ 
+    children, 
+    headerLabel, 
+    backButtonLabel, 
+    backButtonHref, 
+    showSocial
+}: AuthCardWrapperProps) => {
+    return (
+        <Card className='w-[400px] shadow-md'>
+            <CardHeader>
+                <AuthHeader label={headerLabel}/>
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <AuthSocial />
+                </CardFooter>
+            )} 
+        </Card>
+    )
+}
