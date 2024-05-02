e70f4a876e917b5f9d99d4b0582652c8140630dd
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-02 18:54:03 +0900

    Schema added to login form

--- components/auth-components/login-form.tsx
+++ components/auth-components/login-form.tsx
@@ -1,4 +1,21 @@
+'use client'
+
+import * as z from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoginSchema } from '@/schemas';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+
 export const LoginForm = () => {
+
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    }); 
+
     return (
         <div>
             Login Form
