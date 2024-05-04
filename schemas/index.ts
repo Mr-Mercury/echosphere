8461530f4d1165bdce0014df03199ba6adb2a091
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-04 15:35:06 +0900

    Registration validator work

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,8 +1,16 @@
 import * as z from 'zod';
+import {passwordValidator} from '@/lib/utils/data/validators/registration-validators'
 
 export const LoginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(1, {
         message: 'Password is Required'
+    })
+});
+
+export const RegistrationSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+        message: 'Password requires at least 8 chars'
     })
 });
