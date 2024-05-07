1ffa24ccf41372505a244fdade6ae1f81b3193ac
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 12:25:14 +0900

    Registration action created, functioning

--- app/actions/register.ts
+++ app/actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
 import * as z from 'zod';
 import { RegistrationSchema } from '@/schemas';
+import bcrypt from "bcrypt";
+import { db } from '@/lib/db/db';
+import { getUserByEmail } from '@/lib/utilities/data/fetching/userData';
 
 
 //Again, type safety for login using zod
@@ -10,6 +13,24 @@ export const registerAction = async (values: z.infer<typeof RegistrationSchema>)
     if (!validatedFields.success) {
         return { error: "Invalid fields!"}
     }
+
+    const { email, password, username } = validatedFields.data;
+    const hashedPw = await bcrypt.hash(password, 11);
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'If email is in use, you will receive a reset password link'}
+    }
+    // MAKE SURE IT'S HASHEDPW PASSED TO THE NEW ACCOUNT!!!!!  DO NOT CHANGE THIS!
+    await db.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPw
+        }
+    });
+
+    
 
     return { success: "Validation email sent!"}
 }
