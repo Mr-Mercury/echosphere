991b9cb1518b4576122d4dbf9bf2aea9c1d2d561
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 19:29:34 +0900

    auth email verification action currently broken, figuring it out

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from "./lib/db/db";
-import { getUserById } from "./lib/utilities/data/fetching/userData";
+import { getUserByEmail, getUserById } from "./lib/utilities/data/fetching/userData";
  
 // Non ideal solution to TS errors, but at least it sorta works
 
@@ -21,6 +21,17 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         }
     },
     callbacks: {
+        async signIn({ user, account }) {
+            //Adjust if you add more providers? 
+            if (account?.provider !== 'credentials') return true;
+
+            const currentUser = await getUserByEmail(user.id);
+
+            //prevents sign in without email verification
+            if (!currentUser?.emailVerified) return false; 
+            
+            return true;
+        },
         async session({ token, session }) {
             console.log({sessionToken: token,})
             if (token.sub && session.user) {
