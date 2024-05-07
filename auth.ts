db62ef3479fcde141ad39c136e435550b5b6480b
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 14:41:05 +0900

    Auth redirect framework into middleware

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from "./lib/db/db";
  
+// edge workaround for prisma
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+    adapter: PrismaAdapter(db),
+    session: { strategy: 'jwt'},
+    ...authConfig,
 })
