10075dd98a3e21daaf194f2df9d91d9204e522f7
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 16:13:18 +0900

    Login route working

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 import * as z from 'zod';
 import { LoginSchema } from '@/schemas';
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from 'next-auth';
 
 
 //Again, type safety for login using zod
@@ -11,5 +14,21 @@ export const loginAction = async (values: z.infer<typeof LoginSchema>) => {
         return { error: "Invalid fields!"}
     }
 
-    return { success: "Logging in!"}
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn('credentials', {
+            email, password, redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin': 
+                    return { error: 'Email or password was incorrect!'}
+                default:
+                    return { error: 'Something broke :('}
+            }
+        }
+        throw error;
+    }
 }
