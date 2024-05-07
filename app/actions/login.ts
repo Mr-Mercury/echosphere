98cd78a144e67364b4d5196a1b9364f02fe125fe
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 19:16:14 +0900

    Token forced creation/refresh for unverified acct.

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -4,6 +4,8 @@ import { LoginSchema } from '@/schemas';
 import { signIn } from '@/auth';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { AuthError } from 'next-auth';
+import { generateVerificationToken } from '@/lib/utilities/data/tokens/token-generation';
+import { getUserByEmail } from '@/lib/utilities/data/fetching/userData';
 
 
 //Again, type safety for login using zod
@@ -16,6 +18,17 @@ export const loginAction = async (values: z.infer<typeof LoginSchema>) => {
 
     const { email, password } = validatedFields.data;
 
+    const existingUser = await getUserByEmail(email);
+
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: 'Invalid Credentials!' }
+    }
+
+    if (!existingUser.emailVerified) {
+        const verificationToken = await generateVerificationToken(existingUser.email);
+        return { success: 'Confirmation email sent!'};
+    }
+    
     try {
         await signIn('credentials', {
             email, password, redirectTo: DEFAULT_LOGIN_REDIRECT
