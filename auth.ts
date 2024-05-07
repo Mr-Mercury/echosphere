54082daafcd414d2d7c8a4d0389d425fa51fc75e
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-07 17:16:05 +0900

    Session cookie middleware work - now pulls username and humanity

--- auth.ts
+++ auth.ts
@@ -2,9 +2,37 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from "./lib/db/db";
+import { getUserById } from "./lib/utilities/data/fetching/userData";
  
 // edge workaround for prisma
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            console.log({sessionToken: token,})
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.username && session.user) {
+                session.user.username = token.username;
+                session.user.human = token.human;
+            }
+
+            return session;
+        },  
+        // Get by ID because it is the PRIMARY KEY, faster query
+        // Extract any necessary info for the session token here. 
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+            if (!existingUser) return token;
+            
+            token.human = existingUser.human; 
+            token.username = existingUser.username;
+            return token;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: 'jwt'},
     ...authConfig,
