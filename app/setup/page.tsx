3ec07fc9fab5e3831d07eb4c4f451befc8a4af77
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-08 20:35:26 +0900

    user account initialization

---
+++ app/setup/page.tsx
@@ -0,0 +1,31 @@
+import { initializeProfile } from "@/lib/utilities/data/profile/initialize-user";
+import { db } from "@/lib/db/db";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+const SetupPage = async () => {
+    const currentUser = await initializeProfile();
+    console.log(currentUser);
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    userId: currentUser.id
+                }
+            }
+        }
+    })
+
+    if (server) return redirect(`chat/server/${server.id}`);
+    
+    return (
+        <div>
+            <div>Create a server</div>
+            <Link href='/chat/server/personal'>Go to Profile</Link>
+        </div>
+    )
+
+}
+
+export default SetupPage;
