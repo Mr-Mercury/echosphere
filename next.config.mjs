891636eca4e14f2b3128184c708bb2644877e630
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-13 16:13:28 +0900

    image upload and delete complete - TODO: Investigate console errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: [
+            'uploadthing.com',
+            'utfs.io',
+        ],
         remotePatterns: [
         {
             protocol: 'https',
