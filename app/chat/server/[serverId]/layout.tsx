e4801686f29e497abe75ac1af7f0e3f4168bf961
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-14 14:58:11 +0900

    Redoing channel layout to fix css errors & add async

--- app/chat/server/[serverId]/layout.tsx
+++ app/chat/server/[serverId]/layout.tsx
@@ -2,31 +2,42 @@ import { getServerById } from "@/lib/utilities/data/fetching/serverData";
 import { redirect } from "next/navigation";
 import { ChannelSideBar } from "@/components/content-sidebar-components/channel-sidebar/channel-sidebar";
 
-
-const ChannelLayout = async ({ children, params }: {
-    children: React.ReactNode,
-    params: {id:string},
+const ChannelLayout = ({
+    children
+}: {
+    children: React.ReactNode;
 }) => {
-
-    // if (!user) {
-    //     return redirectsignin()
-    // }
-
-    const server = await getServerById(params.id);
-
-    if (!server) {
-        return redirect('/')
-    }
-
     return (
-        <div className='h-full'>
-            <div className='hidden md:flex h-full w-60
-            z-20 flex-col fixed inset-y-0'>
-                <ChannelSideBar server={server}/>
-            </div>
-            <main className='h-full md:pl-60'>{children}</main>
+        <div>
+            {children}
         </div>
     )
 }
 
+
 export default ChannelLayout;
+
+
+// const ChannelLayout = async ({ children, params }: {
+//     children: React.ReactNode,
+//     params: {id:string},
+// }) => {
+
+//     const server = await getServerById(params.id);
+
+//     if (!server) {
+//         return redirect('/')
+//     }
+
+//     return (
+//         <div className='h-full'>
+//             <div className='hidden md:flex h-full w-60
+//             z-20 flex-col fixed inset-y-0'>
+//                 <ChannelSideBar server={server}/>
+//             </div>
+//             <main className='h-full md:pl-60'>{children}</main>
+//         </div>
+//     )
+// }
+
+// export default ChannelLayout;
