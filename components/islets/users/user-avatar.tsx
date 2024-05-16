724d901ce54b8d91c632f07cbd6934eb83da7185
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-16 18:14:50 +0900

    Exploded my database trying to add default prisma text values (not allowed to have 1 value, due to keys....?)

--- components/islets/users/user-avatar.tsx
+++ components/islets/users/user-avatar.tsx
@@ -1,7 +1,20 @@
-export const UserAvatar = () => {
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utilities/clsx/utils";
+
+
+interface UserAvatarProps {
+    src?: string;
+    className?: string;
+}
+
+export const UserAvatar = ({
+    src, 
+    className,
+}: UserAvatarProps) => {
+
     return (
-        <div>
-            AVATAR
-        </div>
+        <Avatar className={cn('h-7 w-7 md:h-10 md:w-10', className)}>
+            <AvatarImage src={src}/>
+        </Avatar>
     )
 }
