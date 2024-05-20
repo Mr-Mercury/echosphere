dba57abf09dd8dfef3df9b6746ebf3fd214a9b14
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-05-20 17:40:10 +0900

    Member modal path first attempt

--- app/chat/server/personal/page.tsx
+++ app/chat/server/personal/page.tsx
@@ -1,12 +1,12 @@
 import { PageContainer } from "@/components/page-container";
 import { FriendSidebar } from "@/components/content-sidebar-components/friend-sidebar/friend-sidebar";
 import { auth, signOut } from "@/auth";
+import { currentUser } from "@/lib/utilities/data/fetching/currentUser";
 
 
 export default async function Personal() {
 
-    let session = await auth();
-    let user = session?.user;
+    let user = currentUser();
 
     return (
         <div>
