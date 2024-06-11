85ab8be049683245b966f56d66b1f7a3d83995a7
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-11 19:14:02 +0900

    conversation creation util

---
+++ lib/utilities/conversation.ts
@@ -0,0 +1,64 @@
+import { db } from "../db/db"
+
+export const conversationUtil = async (memberOneId: string, memberTwoId: string) => {
+    let conversation = await findConversation(memberOneId, memberTwoId) || 
+    await findConversation(memberOneId, memberTwoId);
+
+    if (!conversation) {
+        conversation = await createConversation(memberOneId, memberTwoId);
+    }
+
+    return conversation;
+}
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+    try{
+        return await db.conversation.findFirst({
+            where: {
+                AND: [
+                    { memberOneId: memberOneId },
+                    { memberTwoId: memberTwoId },
+                ]
+            },
+            include: {
+                memberOne: {
+                    include: {
+                        user: true
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        user: true
+                    }
+                }
+            }
+        })
+    } catch {
+        return null
+    }
+}
+
+const createConversation = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.create({
+            data: {
+                memberOneId,
+                memberTwoId,
+            },
+            include: {
+                memberOne: {
+                    include: {
+                        user: true
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        user: true
+                    }
+                }
+            }
+        })
+    } catch {
+        return null
+    }
+}
