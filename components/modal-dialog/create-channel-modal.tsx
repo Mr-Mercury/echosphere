974b7915b24c85f9fa040756b2c009e7c6d8e4e7
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-11 15:02:44 +0900

    Channel modal - trying to swap default channel type based on button clicked

--- components/modal-dialog/create-channel-modal.tsx
+++ components/modal-dialog/create-channel-modal.tsx
@@ -22,22 +22,32 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useParams, useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import { useEffect } from 'react';
 
 
 const CreateChannelModal = () => {
-    const { isOpen, onClose, type } = useModal();
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
     const params = useParams();
 
     const isModalOpen = isOpen && type ==='createChannel';
+    const { channelType } = data;
 
     const form = useForm({
         resolver: zodResolver(ChannelSchema),
         defaultValues: {
             name: '',
-            type: ChannelType.TEXT,
+            type: channelType || ChannelType.TEXT,
         }
-    })
+    });
+
+    useEffect(() => {
+        if (channelType) {
+            form.setValue('type', channelType)
+        } else {
+            form.setValue('type', ChannelType.TEXT)
+        }
+    }, [channelType, form])
 
     const isLoading = form.formState.isSubmitting;
 
