7e727208efd229e93a1c2ed0da9310dcff38f68c
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-11 15:53:42 +0900

    Delete channel functionality

---
+++ app/api/channels/[channelId]/route.ts
@@ -0,0 +1,50 @@
+import { db } from "@/lib/db/db";
+import { currentUser } from "@/lib/utilities/data/fetching/currentUser";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: {channelId: string} }
+) {
+    try {
+        const user = await currentUser();
+        if (!user) return new NextResponse('Unauthorized', {status: 401});
+
+        const {searchParams} = new URL(req.url);
+        const serverId = searchParams.get('serverId');
+
+        if (!serverId) return new NextResponse('Server missing', {status: 400});
+
+        if (!params.channelId) return new NextResponse('Channel Id Missing', {status: 400});
+        
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        userId: user.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+                        }
+                    }
+                }
+            },
+            data: {
+                channels: {
+                    delete: {
+                        id: params.channelId,
+                        name: {
+                            not: 'general',
+                        }
+                    }
+                }
+            }
+        })
+        
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log('[CHANNEL DELETE]', error);
+        return new NextResponse('Internal Error', {status: 500})
+    }
+}
