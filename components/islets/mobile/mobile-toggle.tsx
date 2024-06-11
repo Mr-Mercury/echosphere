fc6c475707b3fae5937c354fd18cfb9a45827a3a
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-11 18:45:22 +0900

    Header mobile sheet added

---
+++ components/islets/mobile/mobile-toggle.tsx
@@ -0,0 +1,23 @@
+import { Menu } from "lucide-react"
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { ChannelSidebar } from "@/components/content-sidebar-components/channel-sidebar/channel-sidebar"
+import ChatMainSidebar from "@/components/chat-sidebar-components/chat-main-sidebar"
+export const MobileToggle = ({serverId}: {serverId: string}) => {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant='ghost' size='icon' className='md:hidden'>
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side='left' className='pd-0 flex gap-0'>
+                <div className='w-[72px]'>
+                    <ChatMainSidebar />
+                </div>
+                <ChannelSidebar serverId={serverId} />
+            </SheetContent>
+        </Sheet>
+    )
+}
