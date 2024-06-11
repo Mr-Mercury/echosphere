7f094343e64797aa7a5bdb519d2f5c55d2cb7430
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-11 18:08:30 +0900

    Commit for dinner

--- app/chat/server/[serverId]/[channelId]/page.tsx
+++ app/chat/server/[serverId]/[channelId]/page.tsx
@@ -1,4 +1,17 @@
-const ChannelIdPage = () => {
+import { currentUser } from "@/lib/utilities/data/fetching/currentUser";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
+
+    const user = await currentUser();
+
+    
     return (
         <div>
             Channel ID
