bf99e24da92a9cb34efcafea908e6b52986c6dcb
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-12 18:19:49 +0900

    Socket context creation

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,4 +1,21 @@
 import { Server as  NetServer } from 'http';
 import { NextApiRequest } from 'next';
 import { Server as IoServer } from 'socket.io';
+import { NextApiResponseIoServer } from '@/lib/entities/servers';
 
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseIoServer) => {
+    if (!res.socket.server.io) {
+        const path = '/api/socket/io';
+        const httpServer: NetServer = res.socket.server as any;
+        const io = new IoServer(httpServer, {
+            path: path,
+            addTrailingSlash: false,
+        })
+    }
+}
