842fc61fa344d76546ba429c61bb35b94742a231
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-12 17:32:12 +0900

    Types for IO workaround

--- lib/entities/servers.ts
+++ lib/entities/servers.ts
@@ -1,6 +1,16 @@
 import { Server, Member, User } from "@prisma/client"
-
+import { Server as NetServer, Socket } from 'net';
+import { NextApiResponse } from "next";
+import { Server as SocketIoServer } from 'socket.io'; 
 
 export type ServerWithMembersAndProfiles = Server & {
     members: (Member & { user: User })[];
+};
+
+export type NextApiResponseIoServer = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIoServer
+        }
+    }
 }
