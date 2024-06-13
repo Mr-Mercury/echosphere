cd64d6e2af796ca3fcc465c6bdf88de2a5025f63
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-13 14:50:07 +0900

    Chat input created, bookmark to try text area

---
+++ components/message-window/chat-input.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Form, FormControl, FormField, FormItem } from '../ui/form';
+import { Input } from '../ui/input';
+import { Plus } from 'lucide-react';
+
+interface ChatInputProps {
+    apiUrl: string;
+    query: Record<string, any>;
+    name: string;
+    type: 'dm' | 'channel'
+}
+
+const formSchema = z.object({
+    content: z.string().min(1)
+})
+
+export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
+    
+    const form = useForm<z.infer<typeof formSchema>>({
+        defaultValues: {
+            content: '',
+        }, 
+        resolver: zodResolver(formSchema),
+    });
+
+    const isLoading = form.formState.isSubmitting;
+    const onSubmit = async (value: z.infer<typeof formSchema>) => {
+        console.log(value);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField 
+                    control={form.control} 
+                    name='content' 
+                    render={({field}) => {
+                        return (
+                            <FormItem>
+                                <FormControl>
+                                    <div className='relative p-4 pb-6'>
+                                        <button 
+                                            type='button' 
+                                            onClick={()=>{}}
+                                            className='absolute top-7 left-8 h-[24px] w-[24px] 
+                                            bg-zinc-400 hover:bg-zinc-300 transition rounded-full 
+                                            p-1 flex items-center justify-center'>
+                                            <Plus className='text-[#313338]'/>
+                                        </button>
+                                        <Input disabled={isLoading} 
+                                        className='px-14 py-6 bg-zinc-700/70 border-none 
+                                        border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-200'/>
+                                    </div>
+                                </FormControl>
+                            </FormItem>
+                        )
+                    }}/>
+            </form>
+        </Form>
+    )
+}
