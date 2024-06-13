245869266929bf0645fd51fb55f2e6af64abefdd
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-13 13:37:31 +0900

    Socket provider created and added

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -2,7 +2,8 @@
 
 import { createContext, useContext, useEffect, useState } from 'react';
 import { io as ClientIo } from 'socket.io-client';
-
+// Workaround to access context via socket because context rerenders any dependent
+// components - forces rerenders based on incoming and outgoing data b/c context is updated
 type SocketContextType = {
     socket: any | null;
     isConnected: boolean;
@@ -15,4 +16,38 @@ const SocketContext = createContext<SocketContextType>({
 
 export const useSocket = () => {
     return useContext(SocketContext);
+}
+
+export const SocketProvider = ({children}: { children: React.ReactNode}) => {
+    const [socket, setSocket ] = useState(null);
+    const [isConnected, setIsConnected] = useState(false);
+
+    useEffect(() => {
+        //Sets up the socket instance - env variable so it remains flexible in dev vs deployed
+        const socketInstance = new (ClientIo as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
+            path: '/api/socket/io',
+            addTrailingSlash: false,
+        })
+
+        socketInstance.on('connect', () => {
+            setIsConnected(true);
+        });
+
+        socketInstance.on('disconnect', () => {
+            setIsConnected(false);
+        });
+
+        setSocket(socketInstance);
+
+        //standard cleanup function, could also be named but online docs say this is more concise
+        return () => {
+            socketInstance.disconnect();
+        }
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{socket, isConnected}}>
+            {children}
+        </SocketContext.Provider>
+    )
 }
