c540c10990d61ebb6165f9f95b82ce921ddc890d
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-06-19 16:39:32 +0900

    ts changes

--- message-server/message-auth.ts
+++ message-server/message-auth.ts
@@ -1,13 +1,15 @@
-const { ExpressAuth } = require('@auth/express');
-const { db } = require('./lib/messageDbConnection');
-
-async function currentUser(sessionToken) {
-    if (!sessionToken) return null;
-
-    const session = await ExpressAuth();
-    
-    
-    const id = sessionToken.user.id;
+import { getSession } from "@auth/express"
+import {Request, Response, NextFunction} from '@types/express'
+ 
+export async function authenticatedUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const session = res.locals.session ?? (await getSession(req, authConfig))
+  if (!session?.user) {
+    res.redirect("/login")
+  } else {
+    next()
+  }
 }
-
-
