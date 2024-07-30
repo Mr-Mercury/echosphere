108e617a9e8a2dc1099bd10f6114b32915c0dd73
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-07-30 19:11:46 +0900

    Cursor seeking system for messages created

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -4,19 +4,19 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { useSocket } from "@/components/providers/socket-provider";
 
 interface ChatQueryProps {
-    apiUrl: string;
+    messageApiUrl: string;
     queryKey: string;
     paramKey: 'channelId' | 'conversationId';
     paramValue: string;
 }
 
-export const useChatQuery = ({queryKey, paramKey, apiUrl, paramValue}: ChatQueryProps) => {
+export const useChatQuery = ({queryKey, paramKey, messageApiUrl, paramValue}: ChatQueryProps) => {
     const { isConnected } = useSocket();
 
     const fetchMessages = async ({ pageParam = undefined}) => {
         
         const url = qs.stringifyUrl({
-            url: apiUrl,
+            url: messageApiUrl,
             query: {
                 cursor: pageParam, 
                 [paramKey]: paramValue}
