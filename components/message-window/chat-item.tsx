6811581f3f5472654f83fd71724be45fa53f1cba
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-08-07 16:43:23 +0900

    Chat item editing functionality created, now requires db route

--- components/message-window/chat-item.tsx
+++ components/message-window/chat-item.tsx
@@ -3,9 +3,19 @@
 import { Member, MemberRole, User } from "@prisma/client";
 import { UserAvatar } from "../islets/users/user-avatar";
 import NavTooltip from "../chat-sidebar-components/nav-tooltip";
-import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utilities/clsx/utils";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
+import qs from "query-string";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
 
 interface ChatItemProps {
     id: string;
@@ -28,14 +38,18 @@ const roleIconMap = {
     'ADMIN': <ShieldAlert className='w-4 h-4 ml-2 text-rose-500' />,
 }
 
+const formSchema = z.object({
+    content: z.string().min(1),
+});
+
 export const ChatItem = ({
     id, content, member, timestamp, fileUrl, deleted, 
     currentMember, isUpdated,
     messageApiUrl, socketQuery
 }: ChatItemProps) => {
-    const [isEditing, useIsEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const [isDeleting, setIsDeleting] = useState(false);
-
+    
     const fileType = fileUrl?.split('.').pop();
     const isAdmin = currentMember.role === MemberRole.ADMIN;
     const isModerator = currentMember.role === MemberRole.MODERATOR;
@@ -44,6 +58,46 @@ export const ChatItem = ({
     const canEditMessage = !deleted && isOwner;
     const isPDF = fileType === 'pdf' && fileUrl;
     const isImage = !isPDF && fileUrl;
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            content: content
+        }
+    })
+
+    //Prevents previous message from showing when triggering isEditing (e.g. during live messaging)
+    useEffect(() => {
+        const handleKeyDown = (event: any) => {
+            if (event.key === 'Escape' || event.keyCode=== 27) {
+                setIsEditing(false);
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown)
+    }, []);
+
+    useEffect(() => {
+        form.reset({
+            content: content,
+        })
+    }, [content])
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: `${messageApiUrl}/${id}`,
+                query: socketQuery
+            })
+
+            await axios.patch(url, values);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className='relative group flex items-center hover:bg-black/5 p-4 transition w-full'>
@@ -82,12 +136,73 @@ export const ChatItem = ({
                         </div>
                     )}
                     {!fileUrl && !isEditing && (
-                        <p>
+                        <p className={cn(
+                            'text-sm text-zinc-300',
+                            deleted && 'italic text-zinc-400 text-xs mt-1'
+                        )}>
                             {content}
+                            {isUpdated && !deleted && (
+                                <span className='text-[10px] mx-2 text-zinc-400'>
+                                    (edited)
+                                </span>
+                            )}
                         </p>
+                    )}
+                    {!fileUrl && isEditing && (
+                        <Form {...form}>
+                            <form className='flex items-center w-full gap-x-2 pt-2'
+                            onSubmit={form.handleSubmit(onSubmit)}>
+                                <FormField 
+                                    control={form.control}
+                                    name='content'
+                                    render={({field}) => (
+                                        <FormItem className='flex-1'>
+                                            <FormControl>
+                                                <div className='relative w-full'>
+                                                    <Input disabled={isLoading}
+                                                    className='p-2 bg-zinc-700/75 border-none border-0 focus-visible:ring-0
+                                                    focus-visible:ring-offset-0 text-zinc-200'
+                                                    placeholder='Edited message'
+                                                    {...field}
+                                                    />
+                                                </div>
+
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button disabled={isLoading} size='sm' variant='primary'>
+                                    Save
+                                </Button>
+                            </form>
+                            <span className='text-[10px] mt-1 text-zinc-400'>
+                                Press escape to cancel, hit enter to save
+                            </span>
+                        </Form>
                     )}
                 </div>
             </div>
+            {canDeleteMessage && (
+                <div className='hidden group-hover:flex items-center 
+                gap-x-2 absolute p-1 -top-2 right-5 bg-zinc-800
+                border-gray-800 rounded-sm'>
+                    {canEditMessage && (
+                        <NavTooltip label='Edit'>
+                            <Edit className='cursor-pointer ml-auto w-4 h-4
+                            text-zinc-500 hover:text-zinc-300 transition'
+                            onClick={() => setIsEditing(true)}/>
+                        </NavTooltip>
+                    )}
+                    {canDeleteMessage && (
+                        <NavTooltip label='Edit'>
+                            <Trash className='cursor-pointer ml-auto w-4 h-4
+                            text-zinc-500 hover:text-zinc-300 transition'
+                            onClick={() => setIsDeleting(true)}/>
+                        </NavTooltip>
+                    )}
+                    
+                </div>
+            )}
         </div>
     )
 }
