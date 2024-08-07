faebb4357235e60b2c1106e9c8dbae1b06026580
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2024-08-07 17:44:16 +0900

    edit message & delete message handler logic

--- server-message/src/lib/message-handler.ts
+++ server-message/src/lib/message-handler.ts
@@ -1,7 +1,8 @@
+import { MemberRole } from "@prisma/client";
 import { db } from "./messageDbConnection.js";
 
 //@ts-ignore
-async function messageHandler( userId, serverId, channelId, fileUrl, content) {
+export async function messagePostHandler( userId, serverId, channelId, fileUrl, content) {
     // Save to DB
 
     const server = await db.server.findFirst({
@@ -54,4 +55,115 @@ async function messageHandler( userId, serverId, channelId, fileUrl, content) {
     // Send back response with message
 }
 
-export default messageHandler;
+//@ts-ignore
+export async function messageEditHandler (userId, messageId, serverId, channelId, content, method) {
+    try {
+        const server = await db.server.findFirst({
+            where: {
+                id: serverId as string,
+                members: {
+                    some: {
+                        userId: userId,
+                    }
+                }
+            },
+            include: {
+                members: true,
+            }
+        })
+
+        if (!server) {
+            return {status: 404, error: 'Server not found!'}
+        }
+
+        const channel = await db.channel.findFirst({
+            where: {
+                id: channelId as string,
+                serverId: serverId as string,
+            },
+        });
+
+        if (!channel) {
+            return {status: 404, error: 'Channel not found!'}
+        };
+
+        const member = server.members.find((member) => member.userId === userId);
+
+        if (!member) {
+            return {status: 404, error: 'Member not found!'};
+        }
+
+        let message = await db.message.findFirst({
+            where: {
+                id: messageId as string,
+                channelId: channelId as string,
+            },
+            include: {
+                member: {
+                    include: {
+                        user: true,
+                    }
+                }
+            }
+        })
+
+        if (!message || message.deleted) {
+            return {status: 404, error: 'Message not found!'};
+        }
+
+        const isMessageOwner = message.memberId === member.id;
+        const isAdmin = member.role === MemberRole.ADMIN;
+        const isModerator = member.role === MemberRole.MODERATOR;
+        const canModify = isMessageOwner || isAdmin || isModerator;
+
+        if (!canModify) {
+            return {status: 401, error: 'Unauthorized!'}
+        };
+
+        if (method === 'DELETE') {
+            message = await db.message.update({
+                where: {
+                    id: messageId as string,
+                }, 
+                data: {
+                    fileUrl: null,
+                    content: 'This message has been deleted',
+                    deleted: true,
+                },
+                include: {
+                    member: {
+                        include: {
+                            user: true,
+                        }
+                    }
+                }
+            })
+        }
+
+        if (method === 'EDIT') {
+            if (!isMessageOwner) return {status: 401, error: 'Unauthorized to edit message!'};
+
+            message = await db.message.update({
+                where: {
+                    id: messageId as string,
+                }, 
+                data: {
+                    content,
+                },
+                include: {
+                    member: {
+                        include: {
+                            user: true,
+                        }
+                    }
+                }
+            })
+        }
+
+        const updateKey = `chat:${channelId}:messages:update`;
+
+        return {updateKey, message};
+    } catch (error) {
+        console.log('EDIT MESSAGE HANDLER ERROR', error);
+    }
+}
