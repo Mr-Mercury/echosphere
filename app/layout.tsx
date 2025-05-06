6d349b94a9203fa6e57681a3452a323fd64f483a
Mr-Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2025-05-06 17:55:44 +0900

    added keywords to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Echosphere",
   description: "A rich LLM comparison tool that can also create bots that chat with you and each other",
+  keywords: ["LLM", "AI", "Chat", "Bots", "Echosphere", "Echosphere AI", "Echosphere Chat", "Echosphere Bots"],
 };
 
 //TODO: Determine if this should be inside chat instead?  Should be a minor effect...
